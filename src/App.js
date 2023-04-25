4beca7df775c06aa4e3ab8be29dacb191e446a91
Joliblin <99505204+Joliblin@users.noreply.github.com>
2023-04-25 12:31:07 +0300

    oggylib

--- src/App.js
+++ src/App.js
@@ -29,8 +29,103 @@ function App() {
         </div>
         <div className="gradUp"></div>
       </header>
-      <main className="main"></main>
+      <main className="main">
+        <div className="themeGray"></div>
+        <div className="partOfTable"> <ul className="nameOfTable">
+          <li>название</li>
+          <li>автор</li>
+          <li>дата выдачи</li>
+          <li>дата сдачи</li>
+          <li>статус</li>
+          <li><img src="/img/tableIcon1.png"></img></li>
+        </ul>
+        <table className="table">
+    <tr>
+        <td>название</td>
+        <td>автор</td>
+        <td>10.05.2005</td>
+        <td>23.02.2023</td>
+        <td>сдано</td>
+        <td><img src="/img/tableIcon2.png"></img></td>
+    </tr>
+    <tr>
+        <td>название</td>
+        <td>автор</td>
+        <td>10.05.2005</td>
+        <td>23.02.2023</td>
+        <td>сдано</td>
+        <td><img src="/img/tableIcon2.png"></img></td>
+    </tr>
+    <tr>
+        <td>название</td>
+        <td>автор</td>
+        <td>10.05.2005</td>
+        <td>23.02.2023</td>
+        <td>сдано</td>
+        <td><img src="/img/tableIcon2.png"></img></td>
+    </tr>
+    <tr>
+        <td>название</td>
+        <td>автор</td>
+        <td>10.05.2005</td>
+        <td>23.02.2023</td>
+        <td>сдано</td>
+        <td><img src="/img/tableIcon2.png"></img></td>
+    </tr>
+    <tr>
+        <td>название</td>
+        <td>автор</td>
+        <td>10.05.2005</td>
+        <td>23.02.2023</td>
+        <td>сдано</td>
+        <td><img src="/img/tableIcon2.png"></img></td>
+    </tr>
+    <tr>
+        <td >название</td>
+        <td>автор</td>
+        <td>10.05.2005</td>
+        <td>23.02.2023</td>
+        <td>сдано</td>
+        <td><img src="/img/tableIcon2.png"></img></td>
+    </tr>
+    <tr>
+        <td>название</td>
+        <td>автор</td>
+        <td>10.05.2005</td>
+        <td>23.02.2023</td>
+        <td>сдано</td>
+        <td><img src="/img/tableIcon2.png"></img></td>
+    </tr>
+</table>
+    </div>
+       
+      </main>
       <footer className="footer">
+      <div className="navigationOfHistory">
+        <ul className="listOfNavigation">
+          <li><a><img src="/img/previousPage.png"></img></a></li>
+          <li><a><img src="/img/firstPage.png"></img></a></li>
+          <li><a><img src="/img/twicePage.png"></img></a></li>
+          <li><a><img src="/img/twirdPage.png"></img></a></li>
+          <li><a><img src="/img/fourthPage.png"></img></a></li>
+          <li><a><img src="/img/fifthyPage.png"></img></a></li>
+          <li><a><img src="/img/sixthyPage.png"></img></a></li>
+          <li><a><img src="/img/sevethyPage.png"></img></a></li>
+          <li> <a><img src="/img/nextPage.png"></img></a></li>
+        </ul>
+        <div className="GradPictures d-flex ">
+        <img src="/img/previousPageGrad.png"></img>
+        <img src="img/pagesGrad.png"></img>
+        <img src="img/pagesGrad.png"></img>
+        <img src="img/pagesGrad.png"></img>
+        <img src="img/pagesGrad.png"></img>
+        <img src="img/pagesGrad.png"></img>
+        <img src="img/pagesGrad.png"></img>
+        <img src="img/pagesGrad.png"></img>
+        <img src="/img/nextPageGrad.png"></img>
+        </div>
+
+      </div>
         <div className="gradDown"></div>
       </footer>
     </div>
@@ -38,3 +133,134 @@ function App() {
 }
 
 export default App;
+/*<ul className="d-flex justify-around">
+<li>книга</li>
+<li>автор</li>
+<li>дата выдачи</li>
+<li>дата сдачи</li>
+<li>статус</li>
+<li>картинка</li>
+</ul>
+<ul className="d-flex justify-around">
+<li>книга</li>
+<li>автор</li>
+<li>дата выдачи</li>
+<li>дата сдачи</li>
+<li>статус</li>
+<li>картинка</li>
+</ul>
+<ul className="d-flex justify-around">
+<li>книга</li>
+<li>автор</li>
+<li>дата выдачи</li>
+<li>дата сдачи</li>
+<li>статус</li>
+<li>картинка</li>
+</ul>
+<ul className="d-flex justify-around">
+<li>книга</li>
+<li>автор</li>
+<li>дата выдачи</li>
+<li>дата сдачи</li>
+<li>статус</li>
+<li>картинка</li>
+</ul>
+<ul className="d-flex justify-around">
+<li>книга</li>
+<li>автор</li>
+<li>дата выдачи</li>
+<li>дата сдачи</li>
+<li>статус</li>
+<li>картинка</li>
+</ul>
+<ul className="d-flex justify-around">
+<li>книга</li>
+<li>автор</li>
+<li>дата выдачи</li>
+<li>дата сдачи</li>
+<li>статус</li>
+<li>картинка</li>
+</ul>
+<ul className="d-flex justify-around">
+<li>книга</li>
+<li>автор</li>
+<li>дата выдачи</li>
+<li>дата сдачи</li>
+<li>статус</li>
+<li>картинка</li>
+</ul>
+<ul className="d-flex justify-around">
+<li>книга</li>
+<li>автор</li>
+<li>дата выдачи</li>
+<li>дата сдачи</li>
+<li>статус</li>
+<li>картинка</li>
+</ul>
+/*  <table>
+    <tr>
+        <td>название</td>
+        <td>автор</td>
+        <td>дата выдачи</td>
+        <td>дата сдачи</td>
+        <td>статус</td>
+        <td>иконка</td>
+    </tr>
+    <tr>
+        <td className="indentLines">название</td>
+        <td className="indentLines">автор</td>
+        <td className="indentLines">10.05.2005</td>
+        <td className="indentLines">23.02.2023</td>
+        <td className="indentLines">сдано</td>
+        <td className="indentLines">картинка</td>
+    </tr>
+    <tr>
+        <td className="indentLines">название</td>
+        <td className="indentLines">автор</td>
+        <td className="indentLines">10.05.2005</td>
+        <td className="indentLines">23.02.2023</td>
+        <td className="indentLines">сдано</td>
+        <td className="indentLines">картинка</td>
+    </tr>
+    <tr>
+        <td className="indentLines">название</td>
+        <td className="indentLines">автор</td>
+        <td className="indentLines">10.05.2005</td>
+        <td className="indentLines">23.02.2023</td>
+        <td className="indentLines">сдано</td>
+        <td className="indentLines">картинка</td>
+    </tr>
+    <tr>
+        <td className="indentLines">название</td>
+        <td className="indentLines">автор</td>
+        <td className="indentLines">10.05.2005</td>
+        <td className="indentLines">23.02.2023</td>
+        <td className="indentLines">сдано</td>
+        <td className="indentLines">картинка</td>
+    </tr>
+    <tr>
+        <td className="indentLines">название</td>
+        <td className="indentLines">автор</td>
+        <td className="indentLines">10.05.2005</td>
+        <td className="indentLines">23.02.2023</td>
+        <td className="indentLines">сдано</td>
+        <td className="indentLines">картинка</td>
+    </tr>
+    <tr>
+        <td className="indentLines">название</td>
+        <td className="indentLines">автор</td>
+        <td className="indentLines">10.05.2005</td>
+        <td className="indentLines">23.02.2023</td>
+        <td className="indentLines">сдано</td>
+        <td className="indentLines">картинка</td>
+    </tr>
+    <tr>
+        <td className="indentLines">название</td>
+        <td className="indentLines">автор</td>
+        <td className="indentLines">10.05.2005</td>
+        <td className="indentLines">23.02.2023</td>
+        <td className="indentLines">сдано</td>
+        <td className="indentLines">картинка</td>
+    </tr>
+  
+</table>*/
